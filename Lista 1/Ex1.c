9a2a22ee5776968d79b2007857c231e3938a541c
Thiago Lima Rodrigues <thiagolimarodrigues9@gmail.com>
2023-03-13 11:06:19 -0300

    nova att

--- Lista 1/Ex1.c
+++ Lista 1/Ex1.c
@@ -4,10 +4,10 @@
 
 void ex1(int num){
     if(num % 2 == 1){
-        printf("esse numero %d e impar\n",num);
+        printf("impar");
     }
     else{
-        printf("esse numero %d e par\n",num);
+        printf("par");
     }
 }
 
@@ -15,7 +15,6 @@ int main ()
 {
     int num = 0;
 
-    printf("Enquanto for positivo sera informado se e impar ou par\n");
     while (num >= 0){
         scanf("%d",&num);
         if(num >= 0)
